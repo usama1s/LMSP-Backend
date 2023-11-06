dde356c592a6a6768c7ac1f3fa8c7f24d71ba29d
Muhammad Anoosh <muhammadanoosh.ma@gmail.com>
2023-11-07 01:16:25 +0500

    chore: jwt initialized

--- util/JWT.js
+++ util/JWT.js
@@ -1,33 +1,33 @@
-// const { sign, verify } = require("jsonwebtoken");
-// require('dotenv').config();
+const { sign, verify } = require("jsonwebtoken");
+require('dotenv').config();
 
-// const SECRET = process.env.JWT_SECRET || "123456789abcdefghi";
+const SECRET = process.env.JWT_SECRET || "123456789abcdefghi";
 
-// const generateToken = (user_id) => {
-//     const payload = {
-//         user: user_id
-//     }
-//     const token = sign(payload, SECRET, { expiresIn: "20d" });
-//     return token;
-// };
+const generateToken = (user_id,role) => {
+    const payload = {
+        user: user_id
+    }
+    const token = sign(payload, SECRET, { expiresIn: "20d" });  
+    return token;
+};
 
-// const validateToken = async (req, res, next) => {
+const validateToken = async (req, res, next) => {
 
-//     try {
-//         const authHeader = req.header("Authorization");
-//         if (!authHeader) {
-//             return res.status(401).json("Authorization header missing");
-//         }
-//         const token = authHeader.split(' ')[1]; // Extract the token from the header
-//         const validToken = await verify(token, SECRET);
-//         if (!validToken) {
-//             return res.status(401).json("Token verification failed");
-//         }
-//         req.user = validToken.user;
-//         next();
-//     } catch (error) {
-//         return res.status(500).json("Internal Server Error: " + error.message);
-//     }
-// }
+    try {
+        const authHeader = req.header("Authorization");
+        if (!authHeader) {
+            return res.status(401).json("Authorization header missing");
+        }
+        const token = authHeader.split(' ')[1]; // Extract the token from the header
+        const validToken = await verify(token, SECRET);
+        if (!validToken) {
+            return res.status(401).json("Token verification failed");
+        }
+        req.user = validToken.user;
+        next();
+    } catch (error) {
+        return res.status(500).json("Internal Server Error: " + error.message);
+    }
+}
 
-// module.exports = { generateToken, validateToken };
+module.exports = { generateToken, validateToken };
